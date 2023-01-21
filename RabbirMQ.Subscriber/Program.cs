59cedfdd60a113d82e8279f959c12220e303b9ce
Dev-Fka <dev.fka@gmail.com>
2023-01-21 14:58:14 +0300

    Add project files.

---
+++ RabbirMQ.Subscriber/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+var factory = new ConnectionFactory
+{
+    Port = 5672,
+    Uri = new Uri("amqp://guest:guest@localhost:5672/")
+};
+
+using var conn = factory.CreateConnection(); //Bağlantı oluşturduk
+
+var channel = conn.CreateModel(); // Kanal oluşturduk.
+
+channel.BasicQos(0, 1, false); //her alıcıya 1 mesaj atar 
+
+//channel.QueueDeclare("hello-rabbitmq", true, false, false); // kuyruk resde silinmez,başka kanallardan erişilir,otomatik silinmesin dinleyen olmazsa.
+// dinleyici böyle bir kuyruk yoksa oluşturur.
+
+var consumer = new EventingBasicConsumer(channel);
+
+var a = channel.BasicConsume("hello-rabbitmq", false, consumer); //mesajı verince siler
+
+
+
+consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
+{
+    var msg = Encoding.UTF8.GetString(e.Body.ToArray());
+
+    Thread.Sleep(1000);
+
+    Console.WriteLine("kuyruktran gelen mesaj : " + msg);
+
+    channel.BasicAck(e.DeliveryTag, false); // gönderiiciye mesajı aldım der , 1 taneyi siler.
+};
+
+Console.ReadLine();
---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+var factory = new ConnectionFactory
+{
+    Port = 5672,
+
+    Uri = new Uri("amqp://guest:guest@localhost:5672/")
+};
+
+using var conn = factory.CreateConnection(); //Bağlantı oluşturduk
+
+var channel = conn.CreateModel(); // Kanal oluşturduk.
+
+channel.QueueDeclare("hello-rabbitmq", true, false, false); // kuyruk resde silinmez,başka kanallardan erişilir,otomatik silinmesin dinleyen olmazsa.
+
+Enumerable.Range(1, 50).ToList().ForEach(
+    x =>
+    {
+        string msg = $"Mesaj {x}";
+
+        var msgBody = Encoding.UTF8.GetBytes(msg);
+
+        channel.BasicPublish(string.Empty, "hello-rabbitmq", null, msgBody);
+
+        Console.WriteLine($"Mesaj İletildi.:{msg}");
+
+    }
+    );
+
+
+Console.ReadLine();
